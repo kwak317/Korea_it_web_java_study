2a8d8d628e640c1b3d9f43b1b5f8d63a9c6780ae
Kwak-hojun <hojun6901@gmail.com>
2025-05-16 20:46:34 +0900

    Feat:if

--- src/_07_If/ElseIF.java
+++ src/_07_If/ElseIF.java
@@ -9,29 +9,30 @@ public class ElseIF {
         //80이상이면 "B"
         //70이상이면 "C"
         //나머지는 "D"
-        if (avgScore >= 90){
+        if (avgScore >= 90) {
             System.out.println("등급: A");
-        }else if (avgScore >= 80) {
+        } else if (avgScore >= 80) {
             System.out.println("등급: B");
-        }else if (avgScore >= 70) {
+        } else if (avgScore >= 70) {
             System.out.println("등급: C");
-        }else{
+        } else {
             System.out.println("등급: D");
         }
         //앞에 참이 나온 조건이 있으면 뒤 조건은 무시
 
-    }
-         //중첩 if
-         int age = 23;
-         String gender = "남성";
-         if (age>=20){
-        if (gender.equals("남성")) {
-            System.out.println("성인 남성입니다");
-        } else {
-            System.out.println("성인 여성입니다");
-        }
-    }else {
-        System.out.println("미성년자 입니다.");
-         }
 
+        //중첩 if
+        int age = 23;
+        String gender = "남성";
+        if (age >= 20) {
+            if (gender.equals("남성")) {
+                System.out.println("성인 남성입니다");
+            } else {
+                System.out.println("성인 여성입니다");
+            }
+        } else {
+            System.out.println("미성년자 입니다.");
+        }
+
+    }
 }
