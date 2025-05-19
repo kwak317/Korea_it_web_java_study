009e7816d889e36269858c1963cee1e9d073bec2
Kwak-hojun <hojun6901@gmail.com>
2025-05-19 20:37:46 +0900

    Feat:반복문

---
+++ src/_09_For/For.java
@@ -0,0 +1,44 @@
+package _09_For;
+
+public class For {
+    public static void main(String[] args) {
+        //반복문 - for
+        // for ( 선언; 조건; 증감 ){
+        //      반복될 코드
+        //}
+//        for (int i = 0; i < 10; i++){
+//            System.out.println("안녕" + i);
+//        }
+//
+//        for (int i = 0; i < 50; i++) {
+//            System.out.println("ㅎㅇ");
+//        }
+
+        //1~10까지 출력, 짝수만 출력
+//        for (int i = 2; i < 11; i+=2) {
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 1; i < 11; i++) {
+//            if(i%2==0){
+//                System.out.println(i);
+//            }
+//        }
+//
+//        //10 ~ 1 까지 거꾸로 출력
+//        for (int i = 10; i > 0 ; i--) {
+//            System.out.println(i);
+//        }
+
+        // 문제 -> 1~100까지 다 더한 값을 출력하시오
+        int a = 0;
+        for (int i = 1; i <= 100; i++) {
+            a+=i;
+        }
+        System.out.println(a);
+
+
+
+
+    }
+}
