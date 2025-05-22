d535cb9fd42956f459b62b5f9c000b572fa07e51
Kwak-hojun <hojun6901@gmail.com>
2025-05-22 21:47:28 +0900

    Feat: OOP

---
+++ src/_21_Class/Student/Student.java
@@ -0,0 +1,12 @@
+package _21_Class.Student;
+
+public class Student {
+    String name;
+    int age;
+    String email;
+    String adress;
+
+    public void showInfo(){
+        System.out.println("이름은 "+name+"이고 "+"나이는 "+age+"이며 "+"이메일은 "+ email+"이고 "+"주소는 "+adress+"이다.");
+    }
+}
---
+++ src/_21_Class/Student/StudentMain.java
@@ -0,0 +1,17 @@
+package _21_Class.Student;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.name = "곽호준";
+        student.age = 23;
+        student.email = "hojun6901@gmail.com";
+        student.adress = "부산광역시...";
+
+        student.showInfo();
+
+
+
+    }
+}
